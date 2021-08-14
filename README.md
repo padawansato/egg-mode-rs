eacea3137fb99758632d3f2e65b29beb8ff03a5b
psato <forhighlow@gmail.com>
2021-08-14 15:31:38 +0900

    remove .env file and fix bug

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Note
 - 環境変数を読み込むenv::varに環境変数でtokenを渡すため、実行環境の環境変数に予めtwitter develop acountから取得しておいたtokenを設定しておく必要がある。
+
+- std::env
+  - 環境変数を取得できる。
+- dotenv
+  - 環境変数を.envファイルから取得できる。
--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,10 @@ use tokio::prelude::*;
 async fn main() {
     dotenv().ok();
 
-    let c_key = env::var("CONSUMER_KEY").expect("Please set CONSUMER_KEY");
-    let c_secret = env::var("CONSUMER_SECRET").expect("Please set CONSUMER_SECRET");
-    let a_key = env::var("ACCESS_KEY").expect("Please set ACCESS_KEY");
-    let a_secret = env::var("ACCESS_SECRET").expect("Please set ACCESS_SECRET");
+    let c_key = env::var("CONSUMER_KEY").expect("Please set consumer-key in .env");
+    let c_secret = env::var("CONSUMER_SECRET").expect("Please set consumer-secret in .env");
+    let a_key = env::var("ACCESS_KEY").expect("Please set access-key in .env");
+    let a_secret = env::var("ACCESS_SECRET").expect("Please set access-secret in .env");
     let consumer_token = egg_mode::KeyPair::new(c_key, c_secret);
     let access_token = egg_mode::KeyPair::new(a_key, a_secret);
     let token = egg_mode::Token::Access {
