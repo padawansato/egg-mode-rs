fa1c60447649eac34649a3e867ac6c1b98fb7da3
psato <forhighlow@gmail.com>
2021-08-14 15:06:54 +0900

    fix  main error comment

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,10 @@ use tokio::prelude::*;
 async fn main() {
     dotenv().ok();
 
-    let c_key = env::var("CONSUMER_KEY").expect(".env/consumer-key");
-    let c_secret = env::var("CONSUMER_SECRET").expect(".env/consumer-secret");
-    let a_key = env::var("ACCESS_KEY").expect(".env/access-key");
-    let a_secret = env::var("ACCESS_SECRET").expect(".env/access-secret");
+    let c_key = env::var("CONSUMER_KEY").expect("Please set CONSUMER_KEY");
+    let c_secret = env::var("CONSUMER_SECRET").expect("Please set CONSUMER_SECRET");
+    let a_key = env::var("ACCESS_KEY").expect("Please set ACCESS_KEY");
+    let a_secret = env::var("ACCESS_SECRET").expect("Please set ACCESS_SECRET");
     let consumer_token = egg_mode::KeyPair::new(c_key, c_secret);
     let access_token = egg_mode::KeyPair::new(a_key, a_secret);
     let token = egg_mode::Token::Access {
